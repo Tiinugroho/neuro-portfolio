41ffcd70613c012fc38c2f9969d2d78c7d027652
Jati Nugroho <abynngaming@gmail.com>
2026-05-19 06:58:34 +0700

    fix button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,19 +1,15 @@
 import { ButtonHTMLAttributes } from "react";
-import { cn } from "@/lib/cn";
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button({
   children,
-  className,
+  className = "",
   ...props
 }: Props) {
   return (
     <button
-      className={cn(
-        "neuro-button px-6 py-3 bg-[#ffe600] font-black uppercase text-black hover:-translate-y-1 transition-transform",
-        className
-      )}
+      className={`neuro-button px-6 py-3 bg-[#ffe600] font-black uppercase text-black hover:-translate-y-1 transition-transform ${className}`}
       {...props}
     >
       {children}
